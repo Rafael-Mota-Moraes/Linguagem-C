a2054a1c378fb35e87d1007445f19ddecad3f10e
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-01-06 11:26:13 -0300

    Bubble sort

---
+++ ordenacao-vetores/bubble_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void bubble_sort(int vetor[], int tam)
+{
+    // variável auxiliar
+    int proximo = 0;
+
+    // varrer todo o  vetor externo
+    for (int i = 0; i < tam; i++)
+    {
+        // trabalhar com os próximos elementos
+        for (int j = 0; j < (tam - 1); j++)
+        {
+            // Ocorre a troca
+
+            if (vetor[j] > vetor[j + 1])
+            {
+                proximo = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = proximo;
+            }
+        }
+    }
+}
+
+int main()
+{
+    // vetor desordenado
+    int vetor[6] = {8, 3, 1, 42, 12, 5};
+
+    // função de ordenação utilizando select sort
+    bubble_sort(vetor, 6);
+
+    // apresentar vetor ordenado
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+
+    printf("\n");
+    return 0;
+
+    return 0;
+}
